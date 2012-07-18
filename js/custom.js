7c2f18ca83ff88a5a338211ac02a62a35769edfa
Kyle Burckhard <kyle@vorpalcode.com>
2012-07-18 08:35:04 -0700

    Slim down JS

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,13 @@
-/* Author: Luis Zuno
-   Web: luiszuno.com
-*/
-
-
 // Jquery with no conflict
 jQuery(document).ready(function($) {
-
-	//##########################################
-	// POP IMAGE
-	//##########################################
-	
-	$(".thumbnails a").fancybox({
-		'transitionIn'	:'elastic',
-		'transitionOut'	:'elastic',
-		'speedIn'		:200,
-		'speedOut'		:100,
-		'overlayShow'	:false
-	});
-	
 	//##########################################
 	// TWITTER BOOTSTRAP
 	//##########################################
 	
 	$('.dropdown-toggle').dropdown();
 	
-	$('#topbar').scrollspy();
-	
-	$('#information-tabs a:first').tab('show');
-	
-	
-	
+	$('#topnav').scrollspy();
+
 });
 
 	
